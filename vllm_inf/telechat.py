1ba80f5c80f13caff09a018a41172810375ba8a2
shunxing12345 <168084185+shunxing12345@users.noreply.github.com>
2024-10-18 20:26:24 +0800

    Update telechat.py

--- vllm_inf/telechat.py
+++ vllm_inf/telechat.py
@@ -51,7 +51,8 @@ from vllm.model_executor.layers.vocab_parallel_embedding import (
 from vllm.model_executor.model_loader.weight_utils import (
     default_weight_loader, kv_cache_scales_loader)
 from vllm.model_executor.sampling_metadata import SamplingMetadata
-from vllm.sequence import IntermediateTensors, SamplerOutput
+from vllm.sequence import IntermediateTensors
+from vllm.model_executor.layers.sampler import SamplerOutput
 from vllm.utils import is_hip, print_warning_once
 
 from .interfaces import SupportsLoRA
