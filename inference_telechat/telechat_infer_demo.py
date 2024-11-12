9f69aca88983f768d9262a2fff22c68544388336
shunxing12345 <168084185+shunxing12345@users.noreply.github.com>
2024-11-12 10:38:48 +0800

    Update telechat_infer_demo.py

--- inference_telechat/telechat_infer_demo.py
+++ inference_telechat/telechat_infer_demo.py
@@ -1,36 +1,22 @@
 import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+tokenizer = AutoTokenizer.from_pretrained('TeleChat2/Telechat2-7B', trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained('TeleChat2/Telechat2-7B', trust_remote_code=True, device_map="auto",
+                                                  torch_dtype=torch.float16)
+prompt = "生抽与老抽的区别？"
+messages = [{"role": "user", "content": prompt}]
+text = tokenizer.apply_chat_template(messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
 
-PATH = 'TeleAI/TeleChat2_115B'
-
-
-def main():
-    # 加载模型相关
-    tokenizer = AutoTokenizer.from_pretrained(PATH,trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(PATH, trust_remote_code=True, device_map="auto",
-                                                 torch_dtype=torch.float16)
-    generate_config = GenerationConfig.from_pretrained(PATH)
-    model.eval()
-    # 输入
-    messages = [
-        {"role": "user", "content": "你是谁"},
-        {"role": "bot", "content": "我是telechat"},
-        {"role": "user", "content": "你在干什么"}
-    ]
-
-    text = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
-    )
-    inputs = tokenizer([text], return_tensors="pt")
-    # 推理
-    with torch.no_grad():
-        outputs = model.generate(**inputs, generation_config=generate_config)
-        outputs = outputs[:, inputs['input_ids'].shape[1]:]
-        print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-
-
-if __name__=="__main__":
-    main()
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
