2cf97a8ac794f54ee48b2333bbbed31b8650242a
shunxing12345 <168084185+shunxing12345@users.noreply.github.com>
2024-12-20 10:53:13 +0800

    Update telechat_infer_demo.py

--- inference_telechat/telechat_infer_demo.py
+++ inference_telechat/telechat_infer_demo.py
@@ -1,9 +1,30 @@
 import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
-tokenizer = AutoTokenizer.from_pretrained('TeleChat2/Telechat2-7B', trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained('TeleChat2/Telechat2-7B', trust_remote_code=True, device_map="auto",
-                                                  torch_dtype=torch.float16)
+from transformers import AutoConfig
+
+config = AutoConfig.from_pretrained("TeleChat2/Telechat2-7B")
+config_dict = config.to_dict()
+
+if 'torch_dtype' in config_dict:
+    dtype_str = config_dict['torch_dtype']
+    # 假设config里是"float16"或"float32"这样的字符串
+    if hasattr(torch, dtype_str):
+        model_dtype = getattr(torch, dtype_str)
+    else:
+        raise ValueError(f"torch does not have dtype {dtype_str}")
+else:
+    # 如果没有定义torch_dtype，定义一个默认值
+    model_dtype = torch.float16
+  
+tokenizer = AutoTokenizer.from_pretrained("TeleChat2/Telechat2-7B", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(
+    "TeleChat2/Telechat2-7B", 
+    trust_remote_code=True, 
+    device_map="auto",
+    torch_dtype=model_dtype
+)
+
 prompt = "生抽与老抽的区别？"
 messages = [{"role": "user", "content": prompt}]
 text = tokenizer.apply_chat_template(messages,
